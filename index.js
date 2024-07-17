4e12cbf69ddb08b3c20f8762e5d6e524e83440c4
zaid <mdzaidali2034@gmail.com>
2024-07-17 20:11:26 +0530

    Fifth commit

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require ('express');
 const app = express ();
 
 app.use(express.json())
+const {User, TrackingToken} = require ('./db');
 
-app.get('/',(req,res)=>{
+
+app.get('/',async(req,res)=>{
+    const user=await User.findOne({
+        username:"zaid567"
+    })
+    console.log(user)
     res.json({
-        "message":"hello world"
+        "message":user
     })
 })
 app.listen(3000,()=>{
