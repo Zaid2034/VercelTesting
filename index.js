1a68ab2eb089a2c02d7a3c2036afbd57b4375829
zaid <mdzaidali2034@gmail.com>
2024-07-17 23:27:17 +0530

    45

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const zod = require ('zod');
 const {User, TrackingToken} = require ('./db');
 const jwt = require ('jsonwebtoken');
 const dotenv = require ('dotenv');
-
-app.use (
-  cors ({
-    origin:'*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  })
-);
+app.use(cors())
+// app.use (
+//   cors ({
+//     origin:'*',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//     allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   })
+// );
 dotenv.config ();
 const JWT_SECRET = process.env.JWT_SECRET;
 
