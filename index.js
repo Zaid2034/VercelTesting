949e61dfbddecbed80b95bdb4cf01d33406e05ea
zaid <mdzaidali2034@gmail.com>
2024-07-17 20:43:17 +0530

    tweleve

--- index.js
+++ index.js
@@ -1,73 +1,19 @@
 const express = require ('express');
 const app = express ();
 app.use(express.json())
-const cors = require ('cors');
-// app.use(cors())
-const zod = require ('zod');
-const jwt = require ('jsonwebtoken');
-const dotenv = require ('dotenv');
-const {User, TrackingToken} = require ('./db');
-dotenv.config ();
-const JWT_SECRET = process.env.JWT_SECRET;
-app.use (
-  cors ({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  })
-);
-
-
-const signUpSchema = zod.object ({
-  email: zod.string (),
-  username: zod.string (),
-  password: zod.string (),
-});
+// const cors = require ('cors');
+// app.use (
+//   cors ({
+//     origin: '*',
+//     credentials: true,
+//   })
+// );
 
 app.get('/',async(req,res)=>{
-    const user=await User.findOne({
-        username:"zaid567"
-    })
-    console.log(user)
     res.json({
-        "message":user
+        "message":"Hello World"
     })
 })
-app.post ('/signup', async (req, res) => {
-  console.log ('In signup');
-  console.log ('jwt secret is:', JWT_SECRET);
-  try {
-    const body = req.body;
-    const {success} = signUpSchema.safeParse (req.body);
-    if (!success) {
-      return res.json ({
-        message: 'Email already taken/ Incorrect inputs',
-      });
-    }
-    const user = await User.findOne ({
-      username: body.username,
-    });
-
-    if (user) {
-      return res.json ({
-        message: 'Email already taken/ Incorrect inputs',
-      });
-    }
-    const dbUser = await User.create (body);
-    console.log ('db user:', dbUser.id);
-    const token = jwt.sign (
-      {
-        userId: dbUser._id,
-      },
-      JWT_SECRET
-    );
-    res.json ({
-      message: 'User created successfully',
-      token: token,
-    });
-  } catch (error) {
-    console.log (error);
-  }
-});
 
 app.listen(3000,()=>{
     console.log("Server is running")
