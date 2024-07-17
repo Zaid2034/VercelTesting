8282ae5a386f0ac4355f6820fa7d8de859c9de2b
zaid <mdzaidali2034@gmail.com>
2024-07-17 20:26:14 +0530

    tenth

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const {User, TrackingToken} = require ('./db');
 dotenv.config ();
 const JWT_SECRET = process.env.JWT_SECRET;
 
+const signUpSchema = zod.object ({
+  email: zod.string (),
+  username: zod.string (),
+  password: zod.string (),
+});
+
 app.get('/',async(req,res)=>{
     const user=await User.findOne({
         username:"zaid567"
@@ -19,6 +25,43 @@ app.get('/',async(req,res)=>{
         "message":user
     })
 })
+app.post ('/signup', async (req, res) => {
+  console.log ('In signup');
+  console.log ('jwt secret is:', JWT_SECRET);
+  try {
+    const body = req.body;
+    const {success} = signUpSchema.safeParse (req.body);
+    if (!success) {
+      return res.json ({
+        message: 'Email already taken/ Incorrect inputs',
+      });
+    }
+    const user = await User.findOne ({
+      username: body.username,
+    });
+
+    if (user) {
+      return res.json ({
+        message: 'Email already taken/ Incorrect inputs',
+      });
+    }
+    const dbUser = await User.create (body);
+    console.log ('db user:', dbUser.id);
+    const token = jwt.sign (
+      {
+        userId: dbUser._id,
+      },
+      JWT_SECRET
+    );
+    res.json ({
+      message: 'User created successfully',
+      token: token,
+    });
+  } catch (error) {
+    console.log (error);
+  }
+});
+
 app.listen(3000,()=>{
     console.log("Server is running")
 })
